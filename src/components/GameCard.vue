efe4624e8ff91e12d3438ebbcd9a50c70b4323ba
David <davidalvper@gmail.com>
2024-03-12 18:50:46 +0000

    start with fronted

---
+++ src/components/GameCard.vue
@@ -0,0 +1,16 @@
+<script setup>
+const props = defineProps(['game'])
+</script>
+
+<template>
+  <div class="card" style="width: 18rem">
+    <img :src="props.game.urlImg" class="card-img-top" alt="..." />
+    <div class="card-body">
+      <h5 class="{{props.game.name}}">Card title</h5>
+      <p class="card-text">
+        {{ props.game.platform }}
+      </p>
+      <a :href="props.game.urlPage" class="btn btn-primary">Ir a la tienda</a>
+    </div>
+  </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,27 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import axios from 'axios'
+import { ref } from 'vue'
+import GameCard from '@/components/GameCard.vue'
+
+const GAMES = ref([])
+
+const getGames = async () => {
+  axios
+    .get('http://localhost:8080/api/games')
+    .then((response) => {
+      GAMES.value = response.data
+    })
+    .catch((error) => {
+      console.error('Error al obtener los juegos', error)
+    })
+}
+
+getGames()
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <h1>Games</h1>
+  <template v-for="game in GAMES.value" :key="game.id">
+    <GameCard :game="game" />
+  </template>
 </template>
