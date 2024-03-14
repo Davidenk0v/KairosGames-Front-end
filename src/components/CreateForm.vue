d28ad0cd099400d956401e064186bcf456ea51a6
David <davidalvper@gmail.com>
2024-03-14 20:18:05 +0000

    start with create account view

---
+++ src/components/CreateForm.vue
@@ -0,0 +1,81 @@
+<script setup>
+import { RouterLink } from 'vue-router'
+</script>
+<template>
+  <div class="container-fluid">
+    <form class="row">
+      <!-- USERNAME -->
+      <div class="mb-4 col-md-8 mx-auto text-center">
+        <div class="input-group">
+          <span class="input-group-text" id="username" name="username">@</span>
+          <input
+            type="text"
+            class="form-control"
+            placeholder="Introduce el nombre de usuario"
+            aria-label="Username"
+            aria-describedby="basic-addon1"
+          />
+        </div>
+      </div>
+      <!-- EMAIL -->
+      <div class="mb-3 col-md-8 mx-auto text-center">
+        <div class="input-group">
+          <input
+            type="email"
+            class="form-control"
+            placeholder="Introduce tu email"
+            aria-label="Recipient's email"
+            aria-describedby="basic-addon2"
+          />
+        </div>
+      </div>
+      <!-- PASSWORD -->
+      <div class="mb-3 col-md-8 mx-auto text-center">
+        <label for="basic-url" class="form-label">Introduce tu contraseña:</label>
+        <div class="input-group col-md-8 mx-auto text-center">
+          <input
+            type="password"
+            class="form-control"
+            id="password"
+            aria-describedby="basic-addon3 basic-addon4"
+          />
+        </div>
+        <label for="basic-url" class="form-label">Confirma tu contraseña:</label>
+        <div class="input-group col-md-8 mx-auto text-center">
+          <input
+            type="password"
+            class="form-control"
+            id="password"
+            aria-describedby="basic-addon3 basic-addon4"
+          />
+        </div>
+        <div class="form-text col-md-8 mx-auto text-center" id="basic-addon4">
+          Asegurate de que eliges una contraseña segura
+        </div>
+      </div>
+      <!-- CUARTO GRUPO -->
+      <div class="mb-3 col-md-6 mx-auto text-center">
+        <div class="input-group">
+          <span class="input-group-text">$</span>
+          <input type="text" class="form-control" aria-label="Amount (to the nearest dollar)" />
+          <span class="input-group-text">.00</span>
+        </div>
+      </div>
+      <!-- QUINTO GRUPO -->
+      <div class="mb-3 col-md-8 mx-auto text-center">
+        <div class="input-group">
+          <input type="text" class="form-control" placeholder="Username" aria-label="Username" />
+          <span class="input-group-text">@</span>
+          <input type="text" class="form-control" placeholder="Server" aria-label="Server" />
+        </div>
+      </div>
+      <!-- SEXTO GRUPO -->
+      <div class="col-md-8 mx-auto text-center">
+        <div class="input-group">
+          <span class="input-group-text">With textarea</span>
+          <textarea class="form-control" aria-label="With textarea"></textarea>
+        </div>
+      </div>
+    </form>
+  </div>
+</template>
--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -3,34 +3,36 @@ import { RouterLink } from 'vue-router'
 </script>
 <template>
   <!-- EMAIL DEL USUARIO -->
-  <div class="mb-3 mt-5 row">
-    <label for="relleno" class="col-sm-2 col-form-label"></label>
-    <div class="col-sm-8">
-      <label for="inputPassword">Email</label>
-      <input type="password" class="form-control" id="inputPassword" />
+  <div class="container-fluid h-100 pb-5">
+    <div class="pt-5 mb-3 mt-5 row">
+      <div class="col-sm-3 col-form-label"></div>
+      <div class="col-sm-5">
+        <label for="inputPassword">Email</label>
+        <input type="password" class="form-control" id="inputPassword" />
+      </div>
     </div>
-  </div>
-  <!-- PASSWORD DEL USUARIO -->
-  <div class="mb-3 row">
-    <label for="relleno" class="col-sm-2 col-form-label"></label>
-    <div class="col-sm-8">
-      <label for="inputPassword">Password</label>
-      <input type="password" class="form-control" id="inputPassword" />
+    <!-- PASSWORD DEL USUARIO -->
+    <div class="mb-3 row">
+      <div class="col-sm-3 col-form-label"></div>
+      <div class="col-sm-5">
+        <label for="inputPassword">Password</label>
+        <input type="password" class="form-control" id="inputPassword" />
+      </div>
     </div>
-  </div>
-  <!-- BOTONES -->
-  <div class="mb-3 row">
-    <label for="relleno" class="col-sm-2 col-form-label"></label>
-    <div class="col-sm-8">
-      <button type="button" class="btn btn-success">Entrar</button>
-      <router-link class="btn btn-link" to="/create-account">Crear cuenta</router-link>
+    <!-- BOTONES -->
+    <div class="mb-3 row">
+      <div class="col-sm-3 col-form-label"></div>
+      <div class="col-sm-8">
+        <button type="button" class="btn btn-success">Entrar</button>
+        <router-link class="btn btn-link" to="/create-account">Crear cuenta</router-link>
+      </div>
     </div>
-  </div>
-  <!-- OLVIDE MI CONTRASEÑA -->
-  <div class="mb-3 row">
-    <label for="relleno" class="col-sm-2 col-form-label">Password</label>
-    <div class="col-sm-8">
-      <router-link class="btn btn-link" to="/forgot-password">Olvidé mi contraseña</router-link>
+    <!-- OLVIDE MI CONTRASEÑA -->
+    <div class="mb-3 row">
+      <div class="col-sm-3 col-form-label"></div>
+      <div class="col-sm-8">
+        <router-link class="btn btn-link" to="/forgot-password">Olvidé mi contraseña</router-link>
+      </div>
     </div>
   </div>
 </template>
